9ea66e719ede268314d6f211fe8380d29e4676be
Yokwara <86111332+Yokwara@users.noreply.github.com>
2021-07-18 00:54:22 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,1 +1,13 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "yok2552";
+$dbname = "vanrental";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
