88391754b54308398f80e01620b0b4e7dbe9f050
Yokwara <86111332+Yokwara@users.noreply.github.com>
2021-07-18 00:54:36 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -6,7 +6,7 @@ $dbname = "vanrental";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
