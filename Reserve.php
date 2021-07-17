0020721327b95a1d9fd1281134b8f24b958ea608
Yokwara <86111332+Yokwara@users.noreply.github.com>
2021-07-18 00:38:48 +0700

    Create Reserve.php

---
+++ Reserve.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minimal Dropdown Menu </title>
+    <link rel="stylesheet" href="css/style.css"> 
+</head>
+<body>
+    <header>
+        <div class="nav">
+            <ul>
+                <li class="home"><a href="Home.html">Home</a></li>
+                <li class="Price"><a href="price.html">Price</a></li>
+                <li class="Reserve"><a href="reserve.html">Reserve</a></li>
+            </ul>
+        </div>
+    </header>
+</body>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+
+   
+<h1>VAN PRICE RATE</h1>
+
+<p>Welcome to Van rental!
+
+<p> This is our promotion!!&hellip;
+
+
+<p>  Van 	      >>   1800 baht	 for     15	    peoples        
+<p>  Van / NGV    >>   1500 baht	 for     12	    peoples    
+<p>  Van / LPG    >>   1500 baht	 for     12	    peoples
+    
+
+<address>Made 3/7/2021<br>
+  by myself.</address>
+
+
+
+</body>
+</html>
+
+
+
+<body>
+
+<div class="container">
+  <form method="POST">
+    <div class="form-group">
+      <label for="Rent_number">Number of car</label>
+      <input type="number" required min="1" max="99" size="7" class="form-control" id="Rent_number" placeholder="Enter number of rent" name="number">
+    </div>
+    <div class="form-group">
+      <label for="Customer_name">Customer name</label>
+      <input type="text" required maxlength="20" class="form-control" id="Customer_name" placeholder="Enter Customer name" name="Customer_name">
+    </div>
+    <div class="form-group">
+      <label for="Customer_number">Customer phone number</label>
+      <input type="number" required min="0800000000" max="0999999999" size="7" class="form-control" id="Customer_number" placeholder="Enter number" name="number">
+    </div>
+    <div class="form-group">
+      <label for="Van_pricerate">Price rate </label>
+     <input type="text" required maxlength="20" class="form-control" id="Van_pricerate" placeholder="Enter Price rate" name="Van_pricerate">
+    </div>
+    </div>
+
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+
+
+
+ </body>
+ </html>
